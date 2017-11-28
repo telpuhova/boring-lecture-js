7986c65f734fc7ca70410c76c9372c982803e7d2
Natalia Telpukhova and Ryan Ferris <ryan@>
2017-11-28 10:36:09 -0800

    add parapgraph styling on click

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $("#p1").removeClass();
+    $("#p1").addClass("highlight");
+  });
+  $("#p2").click(function() {
+    $("#p2").removeClass();
+    $("#p2").addClass("highlight");
+  });
+  $("#p3").click(function() {
+    $("#p3").removeClass();
+    $("#p3").addClass("highlight");
+  });
+  $("#p4").click(function() {
+    $("#p4").removeClass();
+    $("#p4").addClass("highlight");
+  });
+});
